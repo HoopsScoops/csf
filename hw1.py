13460e364502dee4112fa59ecec0c4be0e6b390b
hooian24 <hooian24@evergreen.edu>
2013-10-03 13:51:35 -0500

    First homework
    First homework assignment and required files

---
+++ hw1.py
@@ -0,0 +1,45 @@
+# Name: ...
+# Evergreen Login: ...
+# Computer Science Foundations
+# Programming as a Way of Life
+# Homework 1
+
+# You may do your work by editing this file, or by typing code at the
+# command line and copying it into the appropriate part of this file when
+# you are done.  When you are done, running this file should compute and
+# print the answers to all the problems.
+
+import math                     # makes the math.sqrt function available
+###
+### Problem 1
+###
+
+print "Problem 1 solution follows:"
+
+solution1 = -5.86 + math.sqrt(pow(5.86,2) - (4*1*8.5408))
+solution2 = -5.86 - math.sqrt(pow(5.86,2) - (4*1*8.5408))
+print solution1
+print solution2
+
+###
+### Problem 2
+###
+
+print "Problem 2 solution follows:"
+
+import hw1_test
+
+###
+### Problem 3
+###
+
+print "Problem 3 solution follows:"
+
+# ... write your code and comments here (and remove this line)
+
+
+###
+### Collaboration
+###
+
+# ... List your collaborators here, as a comment (on a line starting with "#").
